1738ca247385a2e6e95f317ae4f8bdae75390f48
Philip <philip@deewhy.ie>
2025-02-20 15:27:23 -0500

    fixed tests

---
+++ sdxl/sdxl-studio/client/backend/src/__tests__/routes/image-saferty-check.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import imageSafetyCheck from '../../services/image-safety-check'// Update with the correct path
+import { getSafetyCheckConfig } from '../../utils/config';
+
+jest.mock('axios');
+jest.mock('../../utils/config', () => ({
+  getSafetyCheckConfig: jest.fn(),
+}));
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('imageSafetyCheck', () => {
+  it('should return the safety check response when successful', async () => {
+    (getSafetyCheckConfig as jest.Mock).mockReturnValue({
+        safetyCheckEndpointURL: 'https://mockapi.com',
+        safetyCheckModel: 'mockModel',
+        safetyCheckEndpointToken: 'mockToken',
+      });
+
+
+    const mockResponse = {
+      data: {
+        outputs: [{ data: [false] }],
+      },
+    };
+    mockedAxios.post.mockResolvedValue(mockResponse);
+
+    const result = await imageSafetyCheck('mockImageString');
+    expect(result).toBe(false);
+    expect(axios.post).toHaveBeenCalledWith(
+      'https://mockapi.com/v2/models/mockModel/infer',
+      {
+        inputs: [
+          {
+            name: 'image',
+            shape: [1, 1],
+            datatype: 'String',
+            data: ['mockImageString'],
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: 'Bearer mockToken',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  });
+
+  it('should return true when response is missing expected data', async () => {
+      (getSafetyCheckConfig as jest.Mock).mockReturnValue({
+      safetyCheckEndpointURL: 'https://mockapi.com',
+      safetyCheckModel: 'mockModel',
+      safetyCheckEndpointToken: 'mockToken',
+    });
+
+    mockedAxios.post.mockResolvedValue({ data: {} });
+
+    const result = await imageSafetyCheck('mockImageString');
+    expect(result).toBe(true);
+  });
+});
